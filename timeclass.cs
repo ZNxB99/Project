deccdf3b07d6bdd963e52e460bcc86ccaae14218
ZNxB99 <111533672+ZNxB99@users.noreply.github.com>
2022-11-28 23:16:20 +0700

    Add files via upload

--- timeclass.cs
+++ timeclass.cs
@@ -1,19 +1,19 @@
-public class Time{
-    public Time time = Time.Now;
-    public static DateOnly dateOnly;
-    public static TimeOnly timeOnly;
+public class DateTime{
+    private DateTime datetime = DateTime.Now;
+    private static DateOnly dateOnly;
+    private static TimeOnly timeOnly;
     public static double GetTimetodoble(){
-        timeOnly = TimeOnly.FromDateTime(Time.Now);
+        timeOnly = TimeOnly.FromDateTime(DateTime.Now);
         double hour = timeOnly.Hour;
         double minute = timeOnly.Minute;
         return hour+(minute/100);
     }
     public static DateOnly GetDate(){
-        dateOnly = DateOnly.FromDateTime(Time.Now);
+        dateOnly = DateOnly.FromDateTime(DateTime.Now);
         return dateOnly;
     }
     public static TimeOnly GetTime(){
-        timeOnly = TimeOnly.FromDateTime(Time.Now);
+        timeOnly = TimeOnly.FromDateTime(DateTime.Now);
         return timeOnly;
     }
 }
